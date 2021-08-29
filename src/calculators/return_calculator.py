Fix bug in update calculator method where correct analytics weren't being used in calculation

src/calculators/return_calculator.py
@@ -62,7 +62,7 @@ class ReturnCalculator(AnalyticsCalculator):
         in_scope_prices = (
             fundamentals[-2:]
             if latest_fundamental is None
-            else [fundamentals[-1], latest_fundamental]
+            else [fundamentals[-2], latest_fundamental]
         )
 
         new_return = (np.diff(in_scope_prices) / in_scope_prices[:-1] * 100)[-1]